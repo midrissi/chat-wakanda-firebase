e586dee1a488604048b768401eb9d2f051da1854
Mohamed IDRISSI <mohamedidrissi@MacBook-Air-de-Mohamed.local>
2013-04-28 21:08:35 +0100

    first commit

---
+++ chat/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,78 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+	var room 	= new chat.Room('https://idrissi.firebaseIO.com/'),
+		$chat 	= $('#container1'),
+		$title	= $chat.find('.chat-title'),
+		$content= $chat.find('.content');
+		
+		window.room = room;
+	
+	room.on('new_message', function(body){
+		$content.append($(body));
+		
+		if($chat.hasClass('hided')){
+			$title
+			.addClass('highlighted')
+			.effect("pulsate", { times:3 }, 750);
+		}
+	});
+	
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+	var login1 = {};	// @login
+	var textField1 = {};	// @textField
+	var container2 = {};	// @container
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		$$('textField1').disable();
+		if(waf.directory.currentUser()){
+			$$('textField1').enable();
+		}
+	};// @lock
+
+	login1.logout = function login1_logout (event)// @startlock
+	{// @endlock
+		$$('textField1').disable();
+	};// @lock
+
+	login1.login = function login1_login (event)// @startlock
+	{// @endlock
+		$$('textField1').enable();
+	};// @lock
+
+	textField1.keyup = function textField1_keyup (event)// @startlock
+	{// @endlock
+		if (event.keyCode == 13) {
+			try{
+				room.send(this.getValue());
+				this.setValue('');
+			}
+			catch(e){
+				alert(e.message);
+			}
+		}
+	};// @lock
+
+	container2.click = function container2_click (event)// @startlock
+	{// @endlock
+		if($title.hasClass('highlighted')){
+			$title
+			.stop(true,true)
+			.css('opacity',1)
+			.removeClass('highlighted');
+		}
+		$chat.toggleClass('hided');
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("login1", "logout", login1.logout, "WAF");
+	WAF.addListener("login1", "login", login1.login, "WAF");
+	WAF.addListener("textField1", "keyup", textField1.keyup, "WAF");
+	WAF.addListener("container2", "click", container2.click, "WAF");
+// @endregion
+};// @endlock
---
+++ chat/WebFolder/scripts/chat.js
@@ -0,0 +1,127 @@
+﻿var chat = {};
+
+(function(namespace){
+	function Room(link, helper){
+		if(!link){
+			throw Error('The link is mandatory');
+		}
+		
+		this.link	= link;
+		this.fb		= new Firebase(this.link);
+		this.events	= {};
+		this.helper	= helper || new Helper();
+	}
+	
+	Room.prototype.on = function on(event , fn){
+		var that = this;
+		
+		switch(event){
+			case 'new_message':
+				this.fb.on('child_added', function(ss, prevName){
+					var val = ss.val(),
+						body= that.helper.format(val.name, val.text,ss.name());
+						
+					fn.call(that, body, prevName);
+				});
+				
+				this.events['new_message'] = fn;
+				break;
+//			case 'message_removed':
+//				this.events['message_removed'] = fn;
+//				break;
+//			case 'message_changed':
+//				this.events['message_changed'] = fn;
+//				break;
+//			case 'message_moved':
+//				this.events['message_moved'] = fn;
+//				break;
+		}
+	}
+	
+	Room.prototype.send = function(body){
+		var curUser = waf.directory.currentUser();
+		if(!curUser){
+			throw new Error('You are not connected');
+		}
+		else{
+			this.fb.push({name:curUser.fullName, text:body}).name();
+		}
+	}
+	
+	Room.prototype.getHelper = function(){
+		return this.helper;
+	}
+	
+	function Helper(){
+		
+	}
+	
+	Helper.regex = /:penguin:|:putnam:|:poop:|:\)|:\(|:p|:D|:o|;\)|8\)|8\||>:\(|:\\|:'\(|3:\)|O:\)|:\*|<3|\^_\^|-_-|O.o|>:o|:v|:\|]|:colonthree:|:shark:|:like:|:-\)|:-\(|:-p|:-D|:-o|;-\)|B-\)|8-\)|>:-\(|:-\\|:'-\(|3:-\)|O:-\)|:-\*|>:-o|:-v/g;
+	Helper.smileys = {
+		':)': 'emoticon_smile',
+		':(': 'emoticon_frown',
+		':p': 'emoticon_tongue',
+		':D': 'emoticon_grin',
+		':o': 'emoticon_gasp',
+		';)': 'emoticon_wink',
+		'8)': 'emoticon_glasses',
+		'8|': 'emoticon_sunglasses',
+		'>:(': 'emoticon_grumpy',
+		':\\': 'emoticon_unsure',
+		':\'(': 'emoticon_cry',
+		'3:)': 'emoticon_devil',
+		'O:)': 'emoticon_angel',
+		':*': 'emoticon_kiss',
+		'<3': 'emoticon_heart',
+		'^_^': 'emoticon_kiki',
+		'-_-': 'emoticon_squint',
+		'O.o': 'emoticon_confused',
+		'>:o': 'emoticon_confused_rev',
+		':v': 'emoticon_pacman',
+		':|]': 'emoticon_robot',
+		':colonthree:': 'emoticon_colonthree',
+		':penguin:': 'emoticon_penguin',
+		':putnam:': 'emoticon_putnam',
+		':shark:': 'emoticon_shark',
+		':like:': 'emoticon_like',
+		':poop:': 'emoticon_poop',
+		':-)': 'emoticon_smile',
+		':-(': 'emoticon_frown',
+		':-p': 'emoticon_tongue',
+		':-D': 'emoticon_grin',
+		':-o': 'emoticon_gasp',
+		';-)': 'emoticon_wink',
+		'B-)': 'emoticon_glasses',
+		'8-)': 'emoticon_sunglasses',
+		'>:-(': 'emoticon_grumpy',
+		':-\\': 'emoticon_unsure',
+		':\'-(': 'emoticon_cry',
+		'3:-)': 'emoticon_devil',
+		'O:-)': 'emoticon_angel',
+		':-*': 'emoticon_kiss',
+		'>:-o': 'emoticon_confused_rev',
+		':-v': 'emoticon_pacman'
+	}
+	
+	Helper.prototype.format = function(name, body, ref){
+		body = body.replace(Helper.regex, function (match) {
+		    return typeof Helper.smileys[match] != 'undefined' ?
+				'<span class="emoticon ' + Helper.smileys[match] + '"></span>' : match;
+	  	});
+	  	
+	  	var res = $('<div/>')
+		.html(body)
+		.prepend($('<em/>')
+		.append('<b>')
+		.text(name + ': '));
+		
+		if(ref){
+			res.attr('data-name',ref);
+		}
+		
+		return res.get(0).outerHTML;
+	}
+	
+	namespace.Room 		= Room;
+	namespace.Helper 	= Helper;
+})(chat)
